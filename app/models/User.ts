e2818092847771cd00f2905ee2fdd6816d9af018
Priyanka@20067 <priyankam18042006@gmail.com>
2025-06-02 10:17:05 +0530

    user db

---
+++ app/models/User.ts
@@ -0,0 +1,9 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String },
+  role: { type: String, default: 'USER' },
+}, { timestamps: true });
+
+export default mongoose.models.User || mongoose.model('User', userSchema);
