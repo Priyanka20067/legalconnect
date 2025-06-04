37618bc8498cf7b25b691b830ceffcfb8ac578ae
Priyanka@20067 <priyankam18042006@gmail.com>
2025-06-04 15:42:24 +0530

    add field

--- app/models/User.ts
+++ app/models/User.ts
@@ -4,6 +4,7 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   name: { type: String },
   role: { type: String, default: 'USER' },
+  password: { type: String, required: true }, // Add this field
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
